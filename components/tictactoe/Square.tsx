8c154a3ea14965db3f828e8ccf5146adb90f4f04
scarmonger <psikomania@yahoo.com>
2023-09-03 19:02:06 +0700

    lifting state up

--- components/tictactoe/Square.tsx
+++ components/tictactoe/Square.tsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
